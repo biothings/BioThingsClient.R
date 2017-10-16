d84038224e17af2b00434e2dc2fb225c8defc99a
thomascjohnson <thomascjohnson@gmail.com>
2017-10-16 13:54:30 -0400

    Updating test to agree with change to default query return type

--- tests/testthat/test_query.R
+++ tests/testthat/test_query.R
@@ -9,5 +9,5 @@ test_that("Check that queryMany returns the appropriate types", {
   gene_querymany <- queryMany(c('1053_at', '117_at', '121_at', '1255_g_at',
                                 '1294_at'), "gene", scopes = "reporter",
                               species = "human")
-  expect_is(gene_querymany, "data.frame")
+  expect_is(gene_querymany, "list")
 })
